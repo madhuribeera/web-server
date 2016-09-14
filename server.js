1d4c3c3191c7978432b50aafb3ef09eaf28d6695
madhuribeera <madhuri.beera@credencys.com>
2016-09-14 17:36:11 +0530

    Initialize repo

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT ||8080;
+
+var middleware = require('./middleware.js');
+
+
+app.use(middleware.requireAuthnetication)
+app.use(middleware.logger)
+
+
+app.get('/', function(req, res){
+	res.send("Express application.");
+})
+
+app.get('/Aboutus', function(req, res){
+	res.send("This is about us page.");
+})
+
+app.get('/Contactus', function(req, res){
+	res.send("This is about us page.");
+})
+app.get('/help', function(req, res){
+	res.send("This is help page to clarify our queries.");
+})
+
+
+
+app.use(express.static(__dirname + "/process"));
+
+app.listen(8080);
+console.log("checking");
